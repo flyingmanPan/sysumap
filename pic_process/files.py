f60c7ee72da4500a7f2d88b9ac2fd574e52d7621
ZZFM <flyingman_pan@outlook.com>
2017-12-18 09:45:11 +0800

    resize the pic

--- pic_process/files.py
+++ pic_process/files.py
@@ -1,5 +1,4 @@
-filename='data.txt'
-file=open(filename,'w')
-file.write('test\n')
-file.write('aa')
-file.close()
+import cv2
+img = cv2.imread('1.jpg',cv2.CV_8UC1)
+img=cv2.resize(img,(775,865))
+cv2.imwrite('resize.jpg',img)
